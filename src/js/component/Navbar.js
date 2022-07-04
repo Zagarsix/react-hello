088b9ebc1f447b62a3fade4808ddcdd19c15c567
Zagarsix <claudk1006@gmail.com>
2022-07-04 11:33:40 -0400

    actualizar Navbar

---
+++ src/js/component/Navbar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <h5 className="nav-link" aria-current="page" href="#">Start Bootstrap</h5>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+
+}
