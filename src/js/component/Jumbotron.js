7f168d4674ffaf13f0d92a0470635904e1b286cd
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-04 14:08:46 -0400

    Update Jumbotron heading font-weight

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Jumbotron = (props) => {
 	return (
 		<div className="jumbotron m-5">
-			<h1 className="display-4">{props.title}</h1>
+			<h1 className="display-4 fw-bold">{props.title}</h1>
 			<p className="lead">{props.description}</p>
 				<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">{props.buttonLabel}</a>
 		</div>
