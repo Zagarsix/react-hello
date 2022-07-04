fae18525ca17a9feab8a0feea577326dcce7baf5
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-04 14:26:54 -0400

    Final jumbotron Component

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Jumbotron = (props) => {
 	return (
-		<div className="jumbotron my-5 mx-4">
+		<div className="jumbotron py-5 px-4">
 			<h1 className="display-4 fw-bold">{props.title}</h1>
 			<p className="lead" style={{fontSize: 21}}>{props.description}</p>
 				<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">{props.buttonLabel}</a>
