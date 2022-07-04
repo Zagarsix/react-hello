f344d082fe03eb6e2ed1957bff8482b86f2419fc
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-04 14:36:20 -0400

    Update Card Component

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,31 +1,30 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Card = (props) => {
   return (
-    <div className="card" style={{width: '18rem'}}>
+    <div className="card" style={{ width: "18rem" }}>
       <img src={props.img} className="card-img-top" alt={props.imgLabel} />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">
-            {props.description}
-        </p>
-        <a href={props.buttonURL} className="btn btn-primary">
+        <p className="card-text">{props.description}</p>
+        <div class="card-footer">
+          <a href={props.buttonURL} className="btn btn-primary">
             {props.buttonLabel}
-        </a>
+          </a>
+        </div>
       </div>
     </div>
   );
 };
 
-
-Card.propTypes ={
+Card.propTypes = {
   img: PropTypes.string,
   imgLabel: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
   buttonURL: PropTypes.string,
-  buttonLabel: PropTypes.string
-}
+  buttonLabel: PropTypes.string,
+};
 
 export default Card;
