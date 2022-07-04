c46223b250c05dad36eb8b1765ebe8e5b4ef565e
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-04 14:01:14 -0400

    Add Card component

---
+++ src/js/component/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Card = (props) => {
+  return (
+    <div className="card" style={{width: '18rem'}}>
+      <img src={props.img} className="card-img-top" alt={props.imgLabel} />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">
+            {props.description}
+        </p>
+        <a href={props.buttonURL} className="btn btn-primary">
+            {props.buttonLabel}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+
+Card.propTypes ={
+  img: PropTypes.string,
+  imgLabel: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonURL: PropTypes.string,
+  buttonLabel: PropTypes.string
+}
+
+export default Card;
